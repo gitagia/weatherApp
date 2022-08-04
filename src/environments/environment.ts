1d688405fef2e17f9c4a9ec5808b7c0b002bbc78
Gitagia <mcmaq101@gmail.com>
2022-08-04 10:28:20 +0300

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '9ace4c8711msh19cae03021b7883p138c2djsnc290c4d989f3',
 };
 
 /*
